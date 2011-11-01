162d0337e27dcfad3adb9995168ef62f53b5d5e4
Will Richardson <willr@codespantech.com>
2011-11-01 00:25:21 -0500

    initial commit

---
+++ RadioStreamer/RadioStreamerAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  RadioStreamerAppDelegate.h
+//  RadioStreamer
+//
+//  Created by William Richardson on 10/14/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RadioStreamerAppDelegate : NSObject <UIApplicationDelegate>
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
